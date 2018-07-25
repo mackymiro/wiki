1c6dfc34f9e3a6ff6a546ae6a953568510aa239d
mackymiro <ask.macky.miro@gmail.com>
2018-07-25 17:23:03 +0800

    deleted

--- wp-content/plugins/wp-video-posts/inc/flowplayer/wpvp_flowplayer.js
+++
@@ -1,4 +0,0 @@
-jQuery(window).load(function(){
-	var wpvp_swf_location = object_name.swf;
-	flowplayer("a.myPlayer", ""+wpvp_swf_location+"", { clip:{ autoPlay:false, autoBuffering:true }, plugins: { controls: { volume: true } }});
-});
--- wp-content/plugins/xili-language/xili-css/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// do not modify this sub-folder
-// will be overwritten during updating
-?>
